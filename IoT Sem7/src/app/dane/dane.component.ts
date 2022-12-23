6285a5be7ecb1e51088e730ffa3a3eb7496c4b63
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-12-23 03:45:31 +0100

    wykres dzialajacy

--- IoT Sem7/src/app/dane/dane.component.ts
+++ IoT Sem7/src/app/dane/dane.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dane',
@@ -6,11 +7,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dane.component.css'],
   standalone: true
 })
-export class DaneComponent implements OnInit {
+export class DaneComponent implements OnInit, OnDestroy {
+  public chart: any;
+
+  intervalId: any;
+  data1: Array<Number> = []
+  time1: Array<String> = []
+
+  @Input() IP!: String
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.fetchData();
+    }, 500);
+
+    this.createChart()
   }
 
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  createChart(){
+  
+    this.chart = new Chart("myChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: this.time1, 
+	       datasets: [
+          {
+            label: "Sales",
+            data: this.data1,
+            backgroundColor: 'blue'
+          },
+          // {
+          //   label: "Profit",
+          //   data: ['542', '542', '536', '327', '17',
+					// 				 '0.00', '538', '541'],
+          //   backgroundColor: 'limegreen'
+          // }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+  fetchData() {
+    let fetched = this.mockRandomInt();
+
+    this.data1.unshift(fetched);
+    this.time1.unshift(this.getNow())
+    if(this.data1.length>20) {
+      this.data1.pop()
+      this.time1.pop()
+    }
+
+    this.chart.update();
+  }
+
+  mockRandomInt() {
+    let min = 1;
+    let max = 10;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getNow() {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    return `${hour}:${minute}:${second}`
+  }
 }
