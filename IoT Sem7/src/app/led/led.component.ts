eb36eecf8d8dfb90c728ef65848e28ab6321c757
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-12-30 02:22:35 +0100

    led troche backend

--- IoT Sem7/src/app/led/led.component.ts
+++ IoT Sem7/src/app/led/led.component.ts
@@ -10,10 +10,43 @@ import { NgFor,NgClass } from '@angular/common';
 })
 export class LedComponent implements OnInit {
   loop07: Array<Number> = [0,1,2,3,4,5,6,7]
+  current_mode: Boolean = false;
+  current_color: Number = 0xFF0000;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  set_single(x: Number,y: Number) {
+    let panel = document.getElementById(`led${x}${y}`)
+    panel?.setAttribute("style",`background-color: #${this.current_color.toString(16)}`)
+
+    console.log(panel)
+    console.log(this.current_color)
+    console.log(`background-color: #${this.current_color.toString(16)}`)
+
+    //TODO request
+  }
+
+  set_all() {
+
+  }
+
+  clear_all() {
+
+  }
+
+  set_color() {
+
+  }
+
+  switch_mode() {
+
+  }
+
+  get_set_colors() {
+
   }
 
 }
